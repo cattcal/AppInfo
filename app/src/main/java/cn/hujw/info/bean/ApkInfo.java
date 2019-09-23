718b1786a829f335c741c30093fd6bdb7b1a9809
hujiangwei <hujw_android@163.com>
2019-09-24 07:45:07 +0800

    添加获取APkUtils

---
+++ app/src/main/java/cn/hujw/info/bean/ApkInfo.java
@@ -0,0 +1,34 @@
+package cn.hujw.info.bean;
+
+/**
+ * @author: hujw
+ * @date: 2019/9/24
+ * @description: ApkInfo
+ * @email: hujw_android@163.com
+ */
+public class ApkInfo {
+    public String file_name;
+    public String package_name;
+    public int version_code;
+    public String file_path;
+    public int file_size;
+    public String version_name;
+
+    public ApkInfo() {
+        file_name = "";
+        package_name = "";
+        version_code = 0;
+        file_path = "";
+        file_size = 0;
+        version_name = "";
+    }
+
+    public ApkInfo(String title, String path, int size, String pkg_name, String vs_name, int vs_code) {
+        file_name = title;
+        package_name = pkg_name;
+        version_code = vs_code;
+        file_path = path;
+        file_size = size;
+        version_name = vs_name;
+    }
+}
